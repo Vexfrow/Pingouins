0be87d6b90b071555b2ca034a144d920bb085427
Thomas GREGOIRE <thomas.gregoire82@gmail.com>
2023-05-03 16:23:26 +0200

    Rajout sprite pingouins

--- src/main/Vue/BanquiseGraphique.java
+++ src/main/Vue/BanquiseGraphique.java
@@ -1,6 +1,8 @@
 package Vue;
 
 import Model.Jeu;
+import Model.Cases;
+
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -10,27 +12,53 @@ import java.awt.geom.Area;
 import java.awt.image.BufferedImage;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.nio.Buffer;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BanquiseGraphique extends JComponent {
 
-    BufferedImage hexagonePoisson1, hexagonePoisson2, hexagonePoisson3, hexagonePingouinR, hexagoneVide;
+    BufferedImage hPoisson1, hPoisson2, hPoisson3, hVide;
+    BufferedImage hPingouinR, hPingouinR1, hPingouinR2, hPingouinR3;
+    BufferedImage hPingouinB, hPingouinB1, hPingouinB2, hPingouinB3;
+    BufferedImage hPingouinV, hPingouinV1, hPingouinV2, hPingouinV3;
+    BufferedImage hPingouinJ, hPingouinJ1, hPingouinJ2, hPingouinJ3;
 
     private Jeu jeu;
-
-
     private List<Shape> grille;
 
-
-
-    public BanquiseGraphique(Jeu jeu){
+    public BanquiseGraphique(Jeu jeu) {
         this.jeu = jeu;
-        hexagonePoisson1 = chargeImage("casePoissons1");
-        hexagonePoisson2 = chargeImage("casePoissons2");
-        hexagonePoisson3 = chargeImage("casePoissons3");
-        hexagonePingouinR = chargeImage("pingouinRouge");
-        hexagoneVide = chargeImage("caseVide");
+
+        jeu.placePingouin(3, 3);
+
+        //Todo : trouver une meilleure manière que charger toutes les images directement
+        hPoisson1 = chargeImage("casePoissons1");
+        hPoisson2 = chargeImage("casePoissons2");
+        hPoisson3 = chargeImage("casePoissons3");
+        hVide = chargeImage("caseVide");
+
+        hPingouinR = chargeImage("caseRouge");
+        hPingouinR1 = chargeImage("caseRouge1");
+        hPingouinR2 = chargeImage("caseRouge2");
+        hPingouinR3 = chargeImage("caseRouge3");
+
+        hPingouinV = chargeImage("caseVert");
+        hPingouinV1 = chargeImage("caseVert1");
+        hPingouinV2 = chargeImage("caseVert2");
+        hPingouinV3 = chargeImage("caseVert3");
+
+        hPingouinB = chargeImage("caseBleu");
+        hPingouinB1 = chargeImage("caseBleu1");
+        hPingouinB2 = chargeImage("caseBleu2");
+        hPingouinB3 = chargeImage("caseBleu3");
+
+        hPingouinJ = chargeImage("caseJaune");
+        hPingouinJ1 = chargeImage("caseJaune1");
+        hPingouinJ2 = chargeImage("caseJaune2");
+        hPingouinJ3 = chargeImage("caseJaune3");
+
+
         grille = new ArrayList<>(60);
     }
 
@@ -45,20 +73,19 @@ public class BanquiseGraphique extends JComponent {
     }
 
 
-
     //Todo : Verifier l'utilité de cette fonction
     private void tracer(Graphics2D g, Image i, int x, int y, int l, int h) {
         g.drawImage(i, x, y, l, h, null);
     }
 
 
-    public static BufferedImage getTexturedImage(BufferedImage src, Shape shp){
+    public static BufferedImage getTexturedImage(BufferedImage src, Shape shp) {
         Rectangle r = shp.getBounds();
 
         //On récupère une version redimensionnée de l'image
-        Image imageTmp = src.getScaledInstance((int)r.getWidth(), (int)r.getHeight(), BufferedImage.SCALE_REPLICATE);
+        Image imageTmp = src.getScaledInstance((int) r.getWidth(), (int) r.getHeight(), BufferedImage.SCALE_REPLICATE);
         //On crée une nouvelle image bufferisé
-        BufferedImage buffered = new BufferedImage((int)r.getWidth(), (int)r.getHeight(),BufferedImage.TYPE_INT_ARGB);
+        BufferedImage buffered = new BufferedImage((int) r.getWidth(), (int) r.getHeight(), BufferedImage.TYPE_INT_ARGB);
         //On remplace la nouvelle image par la version redimensionnée de l'image que l'on souhaite mettre
         buffered.getGraphics().drawImage(imageTmp, 0, 0, null);
         src.getGraphics().dispose();
@@ -75,7 +102,7 @@ public class BanquiseGraphique extends JComponent {
     }
 
 
-    public void misAJour(Jeu jeu){
+    public void misAJour(Jeu jeu) {
         this.jeu = jeu;
         majPlateau();
     }
@@ -91,29 +118,29 @@ public class BanquiseGraphique extends JComponent {
 
         float largeur = getSize().width;
         float hauteur = getSize().height;
-        float offsetX = (largeur /10f);
-        float offsetY = (hauteur /20f);
+        float offsetX = (largeur / 10f);
+        float offsetY = (hauteur / 20f);
 
         int colonnes = 8;
         int lignes = 8;
 
-        float size = Math.min(((largeur-(offsetX*2)) / colonnes), ((hauteur-(offsetY*2)) / lignes / 0.9f));
-        float radius = size/2f;
+        float size = Math.min(((largeur - (offsetX * 2)) / colonnes), ((hauteur - (offsetY * 2)) / lignes / 0.9f));
+        float radius = size / 2f;
 
         grille.clear();
 
-        Shape hexagone = new Hexagone(new Point((int) (offsetX + radius),(int) (offsetY + radius)), radius).getHexagone();
+        Shape hexagone = new Hexagone(new Point((int) (offsetX + radius), (int) (offsetY + radius)), radius).getHexagone();
 
         for (int ligne = 0; ligne < lignes; ligne++) {
             float offset = 0;
             colonnes = 8;
 
             if (ligne % 2 == 0) {
-                offset = size/2;
+                offset = size / 2;
                 colonnes = 7;
             }
             for (int colonne = 0; colonne < colonnes; colonne++) {
-                AffineTransform at = AffineTransform.getTranslateInstance((colonne * size) + offset, ligne *size*0.9f);
+                AffineTransform at = AffineTransform.getTranslateInstance((colonne * size) + offset, ligne * size * 0.9f);
                 Area area = new Area(hexagone);
                 area = area.createTransformedArea(at);
 
@@ -130,34 +157,25 @@ public class BanquiseGraphique extends JComponent {
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
 
-        BufferedImage bfi = hexagoneVide;
+        BufferedImage bfi = hVide;
 
         int i = 0;
         int j = 0;
         for (Shape cell : grille) {
 
-            if(jeu.getCase(i,j).estMange()){
-                bfi = hexagoneVide;
-            }else if(jeu.getCase(i,j).getNbPoissons() == 1){
-                bfi = hexagonePoisson1;
-            }else if(jeu.getCase(i,j).getNbPoissons() == 2){
-                bfi = hexagonePoisson2;
-            }else if(jeu.getCase(i,j).getNbPoissons() == 3){
-                bfi = hexagonePoisson3;
-            }
 
-            bfi = getTexturedImage(bfi, cell);
+            bfi = getTexturedImage(getBfi(jeu.getCase(i, j)), cell);
             g2d.drawImage(bfi, cell.getBounds().x, cell.getBounds().y, null);
 
             j++;
 
-            if(i%2 == 0){
-                if(j >= 7) {
+            if (i % 2 == 0) {
+                if (j >= 7) {
                     i = i + 1;
                     j = 0;
                 }
-            }else{
-                if(j >= 8) {
+            } else {
+                if (j >= 8) {
                     i = i + 1;
                     j = 0;
                 }
@@ -167,4 +185,33 @@ public class BanquiseGraphique extends JComponent {
         g2d.dispose();
     }
 
+
+    private BufferedImage getBfi(Cases c) {
+        BufferedImage bfi = null;
+        if (c.estMange()) {
+            bfi = hVide;
+        } else if (c.pingouinPresent() == 0) {
+            if (c.getNbPoissons() == 1)
+                bfi = hPoisson1;
+            else if (c.getNbPoissons() == 2)
+                bfi = hPoisson2;
+            else if (c.getNbPoissons() == 3)
+                bfi = hPoisson3;
+        } else if (c.pingouinPresent() == 1) {
+            if (c.getNbPoissons() == 1)
+                bfi = hPingouinR1;
+            else if (c.getNbPoissons() == 2)
+                bfi = hPingouinR2;
+            else if (c.getNbPoissons() == 3)
+                bfi = hPingouinR3;
+        } else if (c.pingouinPresent() == 2) {
+            if (c.getNbPoissons() == 1)
+                bfi = hPingouinB1;
+            else if (c.getNbPoissons() == 2)
+                bfi = hPingouinB2;
+            else if (c.getNbPoissons() == 3)
+                bfi = hPingouinB3;
+        }
+        return bfi;
+    }
 }
